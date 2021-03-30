b96c375ecaf152c726f86ebd54cf22e5b93c6523
ckinsella1 <cgummie@gmail.com>
2021-03-30 22:36:32 +0000

    Removed ()

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -53,7 +53,7 @@ let vowelBonusScore = function(word) {
 let scrabbleScore;
 
 const scoringAlgorithms = 
-[ Object({ name: 'Simple Score', description: 'Each letter is worth 1 point.', scorerFunction: simpleScore()}), Object({ name: 'Bonus Vowels', description: 'Vowels are 3 pts, consonants are 1 pt.', scorerFunction: vowelBonusScore() }), Object({ name: 'Scrabble', description: 'The traditional scoring algorithm.', scorerFunction: oldScrabbleScorer() }) ];
+[ Object({ name: 'Simple Score', description: 'Each letter is worth 1 point.', scorerFunction: simpleScore }), Object({ name: 'Bonus Vowels', description: 'Vowels are 3 pts, consonants are 1 pt.', scorerFunction: vowelBonusScore }), Object({ name: 'Scrabble', description: 'The traditional scoring algorithm.', scorerFunction: oldScrabbleScorer }) ];
 
 function initialPrompt() {
   let word = input.question("Let's play some scrabble! Enter a word: ");
